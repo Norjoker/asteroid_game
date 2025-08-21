584ecbaa62cd7774fb26daaaa1b75f72cfe5bc2c
Stian <201008172+Norjoker@users.noreply.github.com>
2025-08-21 21:17:47 +0200

    Player vissible

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,8 +16,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)/1000
         screen.fill("#000000")
-        pygame.display.flip()
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip() #Render the screen
+        
 
 
 if __name__ == "__main__":
